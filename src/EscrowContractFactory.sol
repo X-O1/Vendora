c6f4887f2dac3286b672844e0ae2f6f024947cf1
X-O1 <series-x-01@protonmail.com>
2023-07-29 23:55:07 -0700

    Added escrow contracts and a factory to deploy new ones for every deal

--- src/EscrowContractFactory.sol
+++ src/EscrowContractFactory.sol
@@ -7,4 +7,8 @@ import {TermHandler} from "./TermHandler.sol";
 import {InitiatorEscrow} from "./InitiatorEscrow.sol";
 import {TerminatorEscrow} from "./TerminatorEscrow.sol";
 
-contract EscrowContractFactory {}
+contract EscrowContractFactory {
+    TermHandler termHandler;
+    InitiatorEscrow initiatorEscrow;
+    TerminatorEscrow terminatorEscrow;
+}
--- src/InitiatorEscrow.sol
+++ src/InitiatorEscrow.sol
@@ -1,5 +1,62 @@
 // SPDX-License-Identifier: MIT
 
+// CONTRACT OBJECTIVES:
+
 pragma solidity ^0.8.19;
 
-contract InitiatorEscrow {}
+contract InitiatorEscrow {
+    /** CUSTOM ERRORS */
+    error Not_Initiator();
+
+    /** STATE VARIABLES */
+
+    address private s_initiator;
+    bool private s_initiatorSet;
+    mapping(bytes32 => address) private s_whitelistedTokens;
+    bytes32[] public s_symbolWanted;
+    uint256[] public s_amountWanted;
+    bytes32[] public s_symbolGiving;
+    uint256[] public s_amountGiving;
+
+    /** EVENTS */
+    event Initiator_Set(bool indexed set, address indexed initiatorsAddress);
+
+    /** CONTRUCTOR */
+    constructor(
+        bytes32[] memory symbolWanted,
+        uint256[] memory amountWanted,
+        bytes32[] memory symbolGiving,
+        uint256[] memory amountGiving
+    ) {
+        s_symbolWanted = symbolWanted;
+        s_amountWanted = amountWanted;
+        s_symbolGiving = symbolGiving;
+        s_amountGiving = amountGiving;
+        s_initiatorSet = false;
+    }
+
+    /** MODIFIERS */
+    modifier onlyInitiator() {
+        if (msg.sender != s_initiator) {
+            revert Not_Initiator();
+        }
+        _;
+    }
+
+    /** FUNCTIONS */
+    // Set the Initiator
+    function setInitiator() public {
+        require(s_initiatorSet == false, "Initiator already set");
+
+        s_initiator = msg.sender;
+        s_initiatorSet = true;
+
+        emit Initiator_Set(s_initiatorSet, s_initiator);
+    }
+
+    // Deposit
+    // function initiatorDeposit() public onlyInitiator {}
+
+    // Withdraw
+    // function terminatorWithdraw() public onlyTerminator {}
+}
