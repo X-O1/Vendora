2e98fa7a60c83ba843972e79888e7227286cf161
X-O1 <series-x-01@protonmail.com>
2023-07-30 23:56:09 -0700

    Building escrow contracts

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -11,42 +11,47 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract Escrow {
     /** CUSTOM ERRORS */
     error Not_Initiator();
-    error Not_Terminator();
+    error Not_Finalizer();
     error Not_Owner();
 
-    struct Trade {
-        bytes32 assetTicker;
-        uint256 assetAmount;
-    }
     /** STATE VARIABLES */
     address private immutable i_owner;
 
-    address public s_initiator;
-    address public s_terminator;
-
-    Trade[] public s_assetsWanted;
-    Trade[] public s_assetsGiving;
-
+    address private s_initiator;
+    address private s_finalizer;
+    address private s_initiatorDeposit;
+    address private s_finalizerDeposit;
     bool private s_initiatorSet;
-    bool private s_terminatorSet;
+    bool private s_finalizerSet;
+    bool private s_termsSet;
+    TradeState private s_tradeState;
 
-    mapping(bytes32 => address) public s_whitelistedTokens;
-    mapping(address => mapping(bytes32 => uint256)) public s_accountBalances;
+    mapping(bytes32 => address) public s_whitelistedERC20;
+    mapping(address => mapping(bytes32 => uint256))
+        public s_initiatorAccountBalances;
+    mapping(address => mapping(bytes32 => uint256))
+        public s_finalizerAccountBalances;
+    mapping(bytes32 => address) public s_givingERC20;
+    mapping(bytes32 => uint256) public s_amountGivingERC20;
+    mapping(bytes32 => address) public s_wantedERC20;
+    mapping(bytes32 => uint256) public s_amountWantedERC20;
 
     /** EVENTS */
     event Initiator_Set(bool indexed set, address indexed initiatorsAddress);
-    event s_TerminatorSet_Set(
-        bool indexed set,
-        address indexed terminatorsAddress
-    );
+    event Finalizer_Set(bool indexed set, address indexed finalizerAddress);
+
+    enum TradeState {
+        LIVE,
+        CLOSED
+    }
 
     /** CONTRUCTOR */
-    constructor(address payable intiator, address payable terminator) {
+    constructor() {
         s_initiatorSet = false;
-        s_terminatorSet = false;
+        s_finalizerSet = false;
+        s_termsSet = false;
+        s_tradeState = TradeState.CLOSED;
         i_owner = msg.sender;
-        s_initiator = intiator;
-        s_terminator = terminator;
     }
 
     /** MODIFIERS */
@@ -56,9 +61,9 @@ contract Escrow {
         }
         _;
     }
-    modifier onlyTerminator() {
-        if (msg.sender != s_terminator) {
-            revert Not_Terminator();
+    modifier onlyFinalizer() {
+        if (msg.sender != s_finalizer) {
+            revert Not_Finalizer();
         }
         _;
     }
@@ -71,16 +76,17 @@ contract Escrow {
 
     /** FUNCTIONS */
     // Whitelist new tokens
-    function whiteListToken(
+    function whiteListERC20(
         bytes32 symbol,
         address tokenAddress
     ) external onlyOwner {
-        s_whitelistedTokens[symbol] = tokenAddress;
+        s_whitelistedERC20[symbol] = tokenAddress;
     }
 
     // Set the Initiator
     function setInitiator() public {
         require(s_initiatorSet == false, "Initiator already set");
+        require(s_finalizerSet == false, "Deal active");
 
         s_initiator = msg.sender;
         s_initiatorSet = true;
@@ -88,42 +94,86 @@ contract Escrow {
         emit Initiator_Set(s_initiatorSet, s_initiator);
     }
 
-    // Set the Terminator
-    function setTerminator() public {
-        require(s_terminatorSet == false, "Terminator already set");
+    // Set the Finalizer
+    function setFinalizer() public {
+        require(s_initiatorSet == true, "Initiator and terms are not set");
+        require(s_finalizerSet == false, "Finalizer already set");
 
-        s_terminator = msg.sender;
-        s_terminatorSet = true;
+        s_finalizer = msg.sender;
+        s_finalizerSet = true;
 
-        emit Initiator_Set(s_terminatorSet, s_terminator);
+        emit Initiator_Set(s_finalizerSet, s_finalizer);
     }
 
-    // Deposit ERC20 tokens
+    // Set trade terms.
+    function addERC20Wanted(
+        address tokenAddress,
+        bytes32 symbol,
+        uint256 amount
+    ) external onlyInitiator {
+        require(s_initiatorSet == true, "Intitiator is not set");
 
+        s_wantedERC20[symbol] = tokenAddress;
+        s_amountWantedERC20[symbol] = amount;
+    }
+
+    function addERC20ToGive(
+        address tokenAddress,
+        bytes32 symbol,
+        uint256 amount
+    ) external onlyInitiator {
+        require(s_initiatorSet == true, "Intitiator is not set");
+
+        s_givingERC20[symbol] = tokenAddress;
+        s_amountGivingERC20[symbol] = amount;
+    }
+
+    // Finalize the terms
+
+    // Initiator Deposit ERC20 tokens
     function initiatorDepositERC20(
         uint256 amount,
         bytes32 symbol
     ) external onlyInitiator {
-        for (uint256 i = 0; i < s_assetsGiving.length; i++) {}
+        require(
+            s_givingERC20[symbol] == s_whitelistedERC20[symbol],
+            "Not agreed upon asset"
+        );
+        require(
+            amount == s_amountGivingERC20[symbol],
+            "Not agreed upon amount"
+        );
 
         // Adding deposit amout to users account balance in protocol
+        s_initiatorAccountBalances[s_initiatorDeposit][symbol] += amount;
+
         // Transfering amount from user to the contract
-        s_accountBalances[msg.sender][symbol] += amount;
-        ERC20(s_whitelistedTokens[symbol]).transferFrom(
+        ERC20(s_givingERC20[symbol]).transferFrom(
             msg.sender,
             address(this),
             amount
         );
     }
 
-    function terminatorDepositERC20(
+    // Finalizer Deposit ERC20 tokens
+    function finalizerDepositERC20(
         uint256 amount,
         bytes32 symbol
-    ) external onlyTerminator {
+    ) external onlyInitiator {
+        require(
+            s_wantedERC20[symbol] == s_whitelistedERC20[symbol],
+            "Not agreed upon asset"
+        );
+        require(
+            amount == s_amountWantedERC20[symbol],
+            "Not agreed upon amount"
+        );
+
         // Adding deposit amout to users account balance in protocol
+        s_finalizerAccountBalances[s_finalizerDeposit][symbol] += amount;
+
         // Transfering amount from user to the contract
-        s_accountBalances[msg.sender][symbol] += amount;
-        ERC20(s_whitelistedTokens[symbol]).transferFrom(
+        ERC20(s_wantedERC20[symbol]).transferFrom(
             msg.sender,
             address(this),
             amount
@@ -131,42 +181,20 @@ contract Escrow {
     }
 
     // Withdraw
-    function withdrawERC20Tokens(uint256 amount, bytes32 symbol) external {
-        // Check if user is trying to withdraw more than they deposited.
-        // Will change this to check if they are only withdrawing what the other user deposited and that they are the items in the terms of trade
-        require(
-            s_accountBalances[msg.sender][symbol] >= amount,
-            "Insufficent funds"
-        );
+    // function withdrawERC20Tokens(uint256 amount, bytes32 symbol) external {
+    //     // Check if user is trying to withdraw more than they deposited.
+    //     // Will change this to check if they are only withdrawing what the other user deposited and that they are the items in the terms of trade
+    //     require(
+    //         s_accountBalances[s_initiatorDeposit][symbol] >= amount,
+    //         "Insufficent funds"
+    //     );
 
-        // Decrease the amount out of their account balance in protocol since they are withdrawing
-        s_accountBalances[msg.sender][symbol] -= amount;
+    //     // Decrease the amount out of their account balance in protocol since they are withdrawing
+    //     s_accountBalances[msg.sender][symbol] -= amount;
 
-        // Transfer the tokens out
-        ERC20(s_whitelistedTokens[symbol]).transfer(msg.sender, amount);
-    }
-
-    // Set trade terms of front end.
-    function wantedAsset(
-        bytes32 ticker,
-        uint256 amount
-    ) external onlyInitiator {
-        s_assetsWanted.push(Trade(ticker, amount));
-    }
-
-    function givingAsset(
-        bytes32 ticker,
-        uint256 amount
-    ) external onlyInitiator {
-        s_assetsGiving.push(Trade(ticker, amount));
-    }
+    //     // Transfer the tokens out
+    //     ERC20(s_whitelistedTokens[symbol]).transfer(msg.sender, amount);
+    // }
 
     /** GET FUNCTIONS */
-    function getWantedAssets() external view returns (Trade[] memory) {
-        Trade[] memory assets;
-        for (uint256 i = 0; i < s_assetsWanted.length; i++) {
-            assets[i] = s_assetsWanted[i];
-        }
-        return assets;
-    }
 }
