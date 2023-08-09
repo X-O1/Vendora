55a3f0eb8dcf8811990f8bbd14f63842482700cc
X-O1 <series-x-01@protonmail.com>
2023-08-08 18:46:59 -0700

    Building ERC721 support

--- src/EscrowERC721.sol
+++ src/EscrowERC721.sol
@@ -3,6 +3,5 @@
 pragma solidity ^0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract EscrowERC721 {}
