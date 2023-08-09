4efc4677b63eceae23c81c779aa06c2bae1b7eff
X-O1 <series-x-01@protonmail.com>
2023-08-08 18:46:23 -0700

    ERC20 tokens are now supported

---
+++ src/EscrowERC721.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract EscrowERC721 {}
