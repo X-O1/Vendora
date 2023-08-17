64476f760fd728fcb22db0c38bd401cb81b5d3fa
X-O1 <series-x-01@protonmail.com>
2023-08-17 12:16:06 -0700

    Refactoring escrow contract

--- src/Vendora.sol
+++ src/Vendora.sol
@@ -12,6 +12,9 @@ contract Vendora {
     event Trade_Started(bytes32 indexed tradeId);
     event Seller_Ready(bool indexed sellerReady);
     event Buyer_Ready(bool indexed buyerReady);
+    event Seller_Met_Terms(bool indexed sellerMetTerms);
+    event Buyer_Met_Terms(bool indexed buyerMetTerms);
+    event Trade_Completed(bool indexed tradeCompleted);
 
     /** STRUCTS */
     struct NftDetails {
@@ -46,6 +49,7 @@ contract Vendora {
         return keccak256(abi.encodePacked(msg.sender, buyer, block.timestamp));
     }
 
+    // START NEW TRADE
     function startTrade(address buyer) external returns (bytes32) {
         bytes32 tradeId = generateTradeId(buyer);
         Terms storage terms = trades[tradeId];
@@ -63,6 +67,7 @@ contract Vendora {
         return tradeId;
     }
 
+    // ADD ASSETS TO TRADE TERMS
     function addErc721(
         bytes32 tradeId,
         address nftAddress,
@@ -129,7 +134,7 @@ contract Vendora {
             : terms.requestedEthAmount = ethAmount;
     }
 
-    // CHECK IF BOTH PARTIES HAVE ASSETS SET IN TERMS
+    // VERIFY BOTH PARTIES HAVE ASSETS SET IN TERMS
     function verifyUserAssets(bytes32 tradeId) external {
         Terms storage terms = trades[tradeId];
         require(terms.buyer != address(0), "This trade does not exist");
@@ -244,9 +249,157 @@ contract Vendora {
             terms.buyerReady = true;
             emit Buyer_Ready(terms.buyerReady);
         }
+    }
 
-        // if (terms.sellerReady == true && terms.buyerReady == true) {
-        //     Vendora.completeTrade(tradeId);
-        // }
+    // DEPOSIT ALL ASSETS SET IN TERMS OF TRADE
+    function depositAssets(bytes32 tradeId) external payable {
+        Terms storage terms = trades[tradeId];
+        require(terms.buyer != address(0), "Trade does not exist");
+        require(
+            msg.sender == terms.seller || msg.sender == terms.buyer,
+            "Not a member of this trade"
+        );
+        require(
+            terms.sellerReady == true && terms.buyerReady == true,
+            "Buyer and Seller are not ready to complete trade"
+        );
+
+        if (msg.sender == terms.seller) {
+            if (terms.offeredEthAmount > 0) {
+                require(
+                    msg.value == terms.offeredEthAmount,
+                    "Send exact amount of ETH set in terms"
+                );
+            }
+
+            uint256 offeredNftListLength = terms.offeredNfts.length;
+            if (offeredNftListLength > 0) {
+                for (uint256 i = 0; i < offeredNftListLength; i++) {
+                    IERC721(terms.offeredNfts[i].nftAddress).safeTransferFrom(
+                        terms.seller,
+                        address(this),
+                        terms.offeredNfts[i].tokenId
+                    );
+                }
+            }
+
+            uint256 offeredErc20ListLength = terms.offeredErc20s.length;
+            if (offeredErc20ListLength > 0) {
+                for (uint256 i = 0; i < offeredErc20ListLength; i++) {
+                    IERC20(terms.offeredErc20s[i].erc20Address).transferFrom(
+                        terms.seller,
+                        address(this),
+                        terms.offeredErc20s[i].amount
+                    );
+                }
+            }
+
+            terms.sellerMetTerms = true;
+            emit Seller_Met_Terms(terms.sellerMetTerms);
+        }
+
+        if (msg.sender == terms.buyer) {
+            if (terms.requestedEthAmount > 0) {
+                require(
+                    msg.value == terms.requestedEthAmount,
+                    "Send exact amount of ETH set in terms"
+                );
+            }
+
+            uint256 requestedNftListLength = terms.requestedNfts.length;
+            if (requestedNftListLength > 0) {
+                for (uint256 i = 0; i < requestedNftListLength; i++) {
+                    IERC721(terms.requestedNfts[i].nftAddress).safeTransferFrom(
+                            terms.buyer,
+                            address(this),
+                            terms.requestedNfts[i].tokenId
+                        );
+                }
+            }
+
+            uint256 requestedErc20ListLength = terms.requestedErc20s.length;
+            if (requestedErc20ListLength > 0) {
+                for (uint256 i = 0; i < requestedErc20ListLength; i++) {
+                    IERC20(terms.requestedErc20s[i].erc20Address).transferFrom(
+                        terms.buyer,
+                        address(this),
+                        terms.requestedErc20s[i].amount
+                    );
+                }
+            }
+            terms.buyerMetTerms = true;
+            emit Buyer_Met_Terms(terms.buyerMetTerms);
+        }
+        if (terms.sellerMetTerms == true && terms.buyerMetTerms == true) {
+            Vendora.completeTrade(tradeId);
+        }
+    }
+
+    // TRANFER ALL ASSETS TO RIGHTFUL OWNER AND COMPLETE THE TRADE
+    function completeTrade(bytes32 tradeId) internal {
+        Terms storage terms = trades[tradeId];
+        require(terms.buyer != address(0), "Trade does not exist");
+        require(
+            msg.sender == terms.seller || msg.sender == terms.buyer,
+            "Not a member of this trade"
+        );
+        require(
+            terms.sellerMetTerms == true && terms.buyerMetTerms == true,
+            "All assets must be deposited to complete the trade"
+        );
+
+        if (terms.offeredEthAmount > 0) {
+            payable(terms.buyer).transfer(terms.offeredEthAmount);
+        }
+        if (terms.requestedEthAmount > 0) {
+            payable(terms.seller).transfer(terms.requestedEthAmount);
+        }
+
+        uint256 offeredNftListLength = terms.offeredNfts.length;
+        if (offeredNftListLength > 0) {
+            for (uint256 i = 0; i < offeredNftListLength; i++) {
+                IERC721(terms.offeredNfts[i].nftAddress).safeTransferFrom(
+                    address(this),
+                    terms.buyer,
+                    terms.offeredNfts[i].tokenId
+                );
+            }
+        }
+
+        uint256 requestedNftListLength = terms.requestedNfts.length;
+        if (requestedNftListLength > 0) {
+            for (uint256 i = 0; i < requestedNftListLength; i++) {
+                IERC721(terms.requestedNfts[i].nftAddress).safeTransferFrom(
+                    address(this),
+                    terms.seller,
+                    terms.requestedNfts[i].tokenId
+                );
+            }
+        }
+
+        uint256 offeredErc20ListLength = terms.offeredErc20s.length;
+        if (offeredErc20ListLength > 0) {
+            for (uint256 i = 0; i < offeredErc20ListLength; i++) {
+                IERC20(terms.offeredErc20s[i].erc20Address).transfer(
+                    terms.buyer,
+                    terms.offeredErc20s[i].amount
+                );
+            }
+        }
+
+        uint256 requestedErc20ListLength = terms.requestedErc20s.length;
+        if (requestedErc20ListLength > 0) {
+            for (uint256 i = 0; i < requestedErc20ListLength; i++) {
+                IERC20(terms.requestedErc20s[i].erc20Address).transfer(
+                    terms.seller,
+                    terms.requestedErc20s[i].amount
+                );
+            }
+        }
+
+        terms.tradeCompleted = true;
+        emit Trade_Completed(terms.tradeCompleted);
+
+        delete trades[tradeId];
     }
 }
