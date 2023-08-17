cde7acb0736982b6c3e8f91914a840d833809cd0
X-O1 <series-x-01@protonmail.com>
2023-08-17 15:37:53 -0700

    Building uint tests for escrow contract

---
+++ src/mocks/MockERC721.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockNft1 is ERC721 {
+    constructor() ERC721("Mock1", "Mock1") {
+        _safeMint(msg.sender, 1);
+    }
+}
+
+contract MockNft2 is ERC721 {
+    constructor() ERC721("Mock2", "Mock2") {
+        _safeMint(msg.sender, 10);
+    }
+}
+
+contract MockNft3 is ERC721 {
+    constructor() ERC721("Mock3", "Mock3") {
+        _safeMint(msg.sender, 11);
+    }
+}
--- test/unit/VendoraTest.t.sol
+++ test/unit/VendoraTest.t.sol
@@ -4,12 +4,40 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Vendora} from "../../src/Vendora.sol";
+import {DeployVendora} from "../../script/DeployVendora.s.sol";
+import {MOCKLINK, MOCKAAVE, MOCKUNI} from "../../src/mocks/MockERC20.sol";
+import {MockNft1, MockNft2, MockNft3} from "../../src/mocks/MockERC721.sol";
 
 contract VendoraTest is Test {
     Vendora vendora;
 
+    MOCKLINK public link;
+    MOCKAAVE public aave;
+    MOCKUNI public uni;
+    address public linkAddress;
+    address public aaveAddress;
+    address public uniAddress;
+
+    MockNft1 public mockNft1;
+    MockNft2 public mockNft2;
+    MockNft3 public mockNft3;
+    address public mockNft1Address;
+    address public mockNft2Address;
+    address public mockNft3Address;
+
     address SELLER = makeAddr("user");
     address BUYER = makeAddr("user2");
 
-    function setUp() external {}
+    function setUp() external {
+        DeployVendora deployVendora = new DeployVendora();
+        vendora = deployVendora.run();
+
+        link = new MOCKLINK();
+        aave = new MOCKAAVE();
+        uni = new MOCKUNI();
+
+        mockNft1 = new MockNft1();
+        mockNft2 = new MockNft2();
+        mockNft3 = new MockNft3();
+    }
 }
