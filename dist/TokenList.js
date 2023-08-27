d75ad2090640fe5f6590bc7580ab0400f7d848ff
X-O1 <series-x-01@protonmail.com>
2023-08-26 19:53:38 -0700

    Upgraded front-end

--- dist/TokenList.js
+++ dist/TokenList.js
@@ -1,23 +1,5 @@
-type Erc721Option = {
-    imgSrc: string;
-    name: string;
-    symbol: string;
-    address: string;
-}
-type Erc1155Option = {
-    imgSrc: string;
-    name: string;
-    symbol: string;
-    address: string;
-}
-type Erc20Option = {
-    imgSrc: string;
-    name: string;
-    symbol: string;
-    address: string;
-}
-
-const defaultErc721s: Erc721Option[] = [
+"use strict";
+const defaultErc721s = [
     {
         imgSrc: "./images/erc721-project-images/rareshipLogo.png",
         name: "Rareships",
@@ -31,7 +13,7 @@ const defaultErc721s: Erc721Option[] = [
         address: "0xe170b5b0d507b3e0ce3d51c043175e0a39f78b9f"
     }
 ];
-const defaultErc1155s: Erc1155Option[] = [
+const defaultErc1155s = [
     {
         imgSrc: "./images/erc155-project-images/bloodcrystal.png",
         name: "Blood Crytals",
@@ -39,7 +21,7 @@ const defaultErc1155s: Erc1155Option[] = [
         address: "0xe170b5b0d507b3e0ce3d51c043175e0a39f78b9f"
     }
 ];
-const defaultErc20s: Erc20Option[] = [
+const defaultErc20s = [
     {
         imgSrc: "./images/erc20-project-images/linktokenimage.png",
         name: "Chainlink",
@@ -47,154 +29,113 @@ const defaultErc20s: Erc20Option[] = [
         address: "0xe170b5b0d507b3e0ce3d51c043175e0a39f78b9f"
     }
 ];
-
 document.addEventListener("DOMContentLoaded", () => {
-    const tokenMenuPopup = document.querySelector(".token-menu-popup") as HTMLDivElement;
-
+    const tokenMenuPopup = document.querySelector(".token-menu-popup");
     defaultErc721s.forEach((option) => {
-        const tokenOptionDiv = document.createElement("div") as HTMLDivElement;
+        const tokenOptionDiv = document.createElement("div");
         tokenOptionDiv.classList.add("token-option");
         tokenMenuPopup.appendChild(tokenOptionDiv);
-
-        const optionImageDiv = document.createElement("div") as HTMLDivElement;
+        const optionImageDiv = document.createElement("div");
         optionImageDiv.classList.add("option-image");
         tokenOptionDiv.appendChild(optionImageDiv);
-
-        const optionImage = document.createElement("img") as HTMLImageElement;
+        const optionImage = document.createElement("img");
         optionImage.src = option.imgSrc;
         optionImageDiv.appendChild(optionImage);
-
-        const optionDetailsDiv = document.createElement("div") as HTMLDivElement;
+        const optionDetailsDiv = document.createElement("div");
         optionDetailsDiv.classList.add("token-details");
         tokenOptionDiv.appendChild(optionDetailsDiv);
-
-        const optionName = document.createElement("div") as HTMLDivElement;
+        const optionName = document.createElement("div");
         optionName.classList.add("option-name");
         optionName.innerHTML = option.name;
-
-        const optionSymbol = document.createElement("div") as HTMLDivElement;
+        const optionSymbol = document.createElement("div");
         optionSymbol.classList.add("option-symbol");
         optionSymbol.innerHTML = option.symbol;
-
         optionDetailsDiv.appendChild(optionName);
         optionDetailsDiv.appendChild(optionSymbol);
-
-        const optionOrderDetailsDiv = document.createElement("div") as HTMLDivElement;
+        const optionOrderDetailsDiv = document.createElement("div");
         optionOrderDetailsDiv.classList.add("erc721-choice-details");
         tokenOptionDiv.appendChild(optionOrderDetailsDiv);
-    
-        const optionTokenId = document.createElement("input") as HTMLInputElement;
+        const optionTokenId = document.createElement("input");
         optionTokenId.type = "text";
         optionTokenId.placeholder = "Token ID";
         optionTokenId.classList.add("option-token-id");
-    
-        const addAssetButton = document.createElement("button") as HTMLButtonElement;
+        const addAssetButton = document.createElement("button");
         addAssetButton.innerHTML = "Add";
         addAssetButton.classList.add("add-asset");
-    
         optionOrderDetailsDiv.appendChild(optionTokenId);
         optionOrderDetailsDiv.appendChild(addAssetButton);
-
-    })
-
-
+    });
     defaultErc1155s.forEach((option) => {
-        const tokenOptionDiv = document.createElement("div") as HTMLDivElement;
+        const tokenOptionDiv = document.createElement("div");
         tokenOptionDiv.classList.add("token-option");
         tokenMenuPopup.appendChild(tokenOptionDiv);
-
-        const optionImageDiv = document.createElement("div") as HTMLDivElement;
+        const optionImageDiv = document.createElement("div");
         optionImageDiv.classList.add("option-image");
         tokenOptionDiv.appendChild(optionImageDiv);
-
-        const optionImage = document.createElement("img") as HTMLImageElement;
+        const optionImage = document.createElement("img");
         optionImage.src = option.imgSrc;
         optionImageDiv.appendChild(optionImage);
-
-        const optionDetailsDiv = document.createElement("div") as HTMLDivElement;
+        const optionDetailsDiv = document.createElement("div");
         optionDetailsDiv.classList.add("token-details");
         tokenOptionDiv.appendChild(optionDetailsDiv);
-
-        const optionName = document.createElement("div") as HTMLDivElement;
+        const optionName = document.createElement("div");
         optionName.classList.add("option-name");
         optionName.innerHTML = option.name;
-
-        const optionSymbol = document.createElement("div") as HTMLDivElement;
+        const optionSymbol = document.createElement("div");
         optionSymbol.classList.add("option-symbol");
         optionSymbol.innerHTML = option.symbol;
-
         optionDetailsDiv.appendChild(optionName);
         optionDetailsDiv.appendChild(optionSymbol);
-
-        const optionOrderDetailsDiv = document.createElement("div") as HTMLDivElement;
+        const optionOrderDetailsDiv = document.createElement("div");
         optionOrderDetailsDiv.classList.add("erc721-choice-details");
         tokenOptionDiv.appendChild(optionOrderDetailsDiv);
-    
-        const optionTokenId = document.createElement("input") as HTMLInputElement;
+        const optionTokenId = document.createElement("input");
         optionTokenId.type = "text";
         optionTokenId.placeholder = "Token ID";
-
         optionTokenId.classList.add("option-token-id");
-
-        const optionAmount = document.createElement("input") as HTMLInputElement;
+        const optionAmount = document.createElement("input");
         optionAmount.placeholder = "Amount";
         optionAmount.type = "text";
         optionAmount.classList.add("option-amount");
-    
-        const addAssetButton = document.createElement("button") as HTMLButtonElement;
+        const addAssetButton = document.createElement("button");
         addAssetButton.innerHTML = "Add";
         addAssetButton.classList.add("add-asset");
-    
         optionOrderDetailsDiv.appendChild(optionTokenId);
         optionOrderDetailsDiv.appendChild(optionAmount);
         optionOrderDetailsDiv.appendChild(addAssetButton);
-
-    })
-
+    });
     defaultErc20s.forEach((option) => {
-        const tokenOptionDiv = document.createElement("div") as HTMLDivElement;
+        const tokenOptionDiv = document.createElement("div");
         tokenOptionDiv.classList.add("token-option");
         tokenMenuPopup.appendChild(tokenOptionDiv);
-
-        const optionImageDiv = document.createElement("div") as HTMLDivElement;
+        const optionImageDiv = document.createElement("div");
         optionImageDiv.classList.add("option-image");
         tokenOptionDiv.appendChild(optionImageDiv);
-
-        const optionImage = document.createElement("img") as HTMLImageElement;
+        const optionImage = document.createElement("img");
         optionImage.src = option.imgSrc;
         optionImageDiv.appendChild(optionImage);
-
-        const optionDetailsDiv = document.createElement("div") as HTMLDivElement;
+        const optionDetailsDiv = document.createElement("div");
         optionDetailsDiv.classList.add("token-details");
         tokenOptionDiv.appendChild(optionDetailsDiv);
-
-        const optionName = document.createElement("div") as HTMLDivElement;
+        const optionName = document.createElement("div");
         optionName.classList.add("option-name");
         optionName.innerHTML = option.name;
-
-        const optionSymbol = document.createElement("div") as HTMLDivElement;
+        const optionSymbol = document.createElement("div");
         optionSymbol.classList.add("option-symbol");
         optionSymbol.innerHTML = option.symbol;
-
         optionDetailsDiv.appendChild(optionName);
         optionDetailsDiv.appendChild(optionSymbol);
-
-        const optionOrderDetailsDiv = document.createElement("div") as HTMLDivElement;
+        const optionOrderDetailsDiv = document.createElement("div");
         optionOrderDetailsDiv.classList.add("erc721-choice-details");
         tokenOptionDiv.appendChild(optionOrderDetailsDiv);
-    
-        const optionAmount = document.createElement("input") as HTMLInputElement;
+        const optionAmount = document.createElement("input");
         optionAmount.type = "text";
         optionAmount.placeholder = "Amount";
         optionAmount.classList.add("option-amount");
-    
-        const addAssetButton = document.createElement("button") as HTMLButtonElement;
+        const addAssetButton = document.createElement("button");
         addAssetButton.innerHTML = "Add";
         addAssetButton.classList.add("add-asset");
-    
         optionOrderDetailsDiv.appendChild(optionAmount);
         optionOrderDetailsDiv.appendChild(addAssetButton);
-
-    })
-
-})
+    });
+});
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,1 @@
 "use strict";
-let age = 20;
